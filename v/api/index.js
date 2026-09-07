43c5690b253cc62a4777420ec77d4decd1c433b8
link-short <dora.family228@gmail.com>
2026-09-07 16:00:02 +0300

    Create index.js

---
+++ v/api/index.js
@@ -0,0 +1,6 @@
+const UrlKriminalHamster = new URLSearchParams(window.location.search);
+const command = UrlKriminalHamste.get('command');
+
+if (command = "kriminalvirus") {
+  window.open("theannoyingsite.com");
+}
