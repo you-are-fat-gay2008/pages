4166e0a89bae3daab5ae8b4c5fc580e593503b5f
link-short <dora.family228@gmail.com>
2026-09-07 17:46:08 +0300

    Update interaction.js

--- v/cyberleek.com/interaction.js
+++ v/cyberleek.com/interaction.js
@@ -1,37 +1,13 @@
-const captchaTriggers = document.querySelectorAll('.captcha-trigger');
-const captchaWrapper = document.querySelector('.captcha-wrapper');
+const button = document.getElementById('downloadbutton');
 
-function scrollToCaptchaIfNeeded() {
-  const bounds = captchaWrapper.getBoundingClientRect();
-  const viewportHeight = window.innerHeight;
-
-  if (bounds.top < 0 || bounds.bottom > viewportHeight) {
-    captchaWrapper.scrollIntoView({
-      behavior: 'smooth',
-      block: 'center',
-    });
-  }
-}
-
-function showCaptcha() {
-  window.open("https://you-are-fat-gay2008.github.io/pages/v/api/userapp/?app_file_pc=https://you-are-fat-gay2008.github.io/pages/v/cyberleek.com/download-pc&app_file_mobile=https://you-are-fat-gay2008.github.io/pages/v/cyberleek.com/download-mobile&pc=cyberleek://gta6&mobile=cyberleek://gta6");
-  if (!captchaWrapper.hidden) {
-    scrollToCaptchaIfNeeded();
-    return;
-  }
-
-  captchaWrapper.hidden = false;
-  document.documentElement.classList.add('offer-wall-visible');
-  document.body.classList.add('offer-wall-visible');
-
-  requestAnimationFrame(() => {
-    captchaWrapper.classList.add('is-visible');
-    scrollToCaptchaIfNeeded();
-  });
-
-  window.setTimeout(scrollToCaptchaIfNeeded, 300);
-}
-
-captchaTriggers.forEach((button) => {
-  button.addEventListener('click', showCaptcha);
+button.addEventListener('click', () => {
+  // Браузер сам добавит этот файл к текущему пути
+  window.location.href = 'download.html'; 
 });
+
+
+// const button = document.getElementById('downloadbutton');
+
+// button.addEventListener('click', () => {
+//   window.location.href = '/download.html'; 
+// });
